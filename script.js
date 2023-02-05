0dcba7cb647c1958d7dff42979f10ab0b5678d28
sriVishalS <vishalS221202@gmail.com>
2023-02-05 10:47:26 +0530

    changes

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 2000);
+}
+
+function showPage() {
+  document.querySelector(".loaderWrapper").style.display = "none";
+}
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 buttons.forEach((item) => {
